db8c33cdbafe0b91f987f6787b022654b69e2cd8
Jimmy Palelil <jimmypalelil@gmail.com>
2018-11-21 10:31:08 -0800

    changed body background color

--- admin.php
+++ admin.php
@@ -1,24 +1,33 @@
 
 <div class='filter-column'>
     <ul class="list-group">
-    <li class="list-group-item adminButtons" ng-class="{'adminButtonClicked': selectedButton == view }"  ng-click="setTableType(view); setButton(view)" ng-repeat="view in views">{{view}} <i ng-show="selectedButton == view" class="fas fa-chevron-circle-right"></i></button>
+    <li class="list-group-item adminButtons" ng-class="{'adminButtonClicked': selectedButton == view }"  ng-click="setTableType(view); setButton(view)" ng-repeat="view in views">{{view}} 
+        <span ng-show="selectedButton == view" class="badge badge-primary badge-pill"><i class="fas fa-chevron-circle-right"></i></span></li>
 </div>
 
 <hr class="my-4" style="width:100%;">
 
 <div class="grid-container">
     <div class="container">
+        <!-- If no items are found from database -->
         {{table === '' ? "No Items FOUND!!!" : ''}}
+
         <table class="table table-hover table-striped table-bordered table-condensed">
+
+        <!-- Table header with sort icon -->
         <thead class="thead-dark">
             <tr>
-                <th ng-show="key !== 'password'" ng-click="setSortKey(key)" ng-repeat="(key, value) in table[0]">{{key | uppercase}} <i ng-if="key == sortKey" class="fas fa-sort-amount-up"></i><i ng-if="key == sortKeyDown" class="fas fa-sort-amount-down"></i></th>
+                <th ng-show="key !== 'password'" ng-click="setSortKey(key)" ng-repeat="(key, value) in table[0]">{{key | uppercase}} 
+                    <i ng-if="key == sortKey" class="fas fa-sort-amount-up"></i><i ng-if="key == sortKeyDown" class="fas fa-sort-amount-down"></i>
+                </th>
                 <th ng-show="tableType=='Get PRODUCTS'">EDIT</th>
             </tr>
         </thead>
         <tr ng-repeat="item in table | orderBy: sortKey">
-            <td ng-show="key !== 'password'" ng-repeat="(key,value) in item">{{ key.includes('price') ? '$' : '' }}{{key === 'total_quantity_sold' && value == null ? '0': value == null ? 'N/A' : value}}
-                        {{(key === 'role' && value === 0) || ((key === 'USER_ID' || key === 'uid') &&  value <= 4) ? '(ADMIN)' : ''}} </td>
+            <td ng-show="key !== 'password'" ng-repeat="(key,value) in item">
+                {{ key.includes('price') ? '$' : '' }}{{key === 'total_quantity_sold' && value == null ? '0': value == null ? 'N/A' : value}}
+                {{(key === 'role' && value === 0) || ((key === 'USER_ID' || key === 'uid') &&  value <= 4) ? '(ADMIN)' : ''}} 
+            </td>
             <td ng-show="tableType=='Get PRODUCTS'"><a data-toggle="modal" data-target="#updateModal" ng-click="setUpdateItem(item)"><i class="fas fa-edit"></i></a>
         </tr>
         </table>
